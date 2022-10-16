5fa114b204512c48ef0cbde1985e502c81f3fd42
André Fazendeiro <andre.fazendeiro@flowfactory.com>
2022-10-16 16:24:36 +0200

    Added material UI functionality

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
+  const [isExpanded, setExpanded] = React.useState(false);
   const [note, setNote] = React.useState({
     title: "",
     content: "",
@@ -16,36 +20,46 @@ function CreateArea(props) {
     });
   }
 
+  function shouldExpand() {
+    setExpanded(true);
+  }
+
   return (
     <div>
-      <form>
-        <input
-          name="title"
-          placeholder="Title"
-          value={note.title}
-          onChange={handleChange}
-        />
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            name="title"
+            placeholder="Title"
+            value={note.title}
+            onChange={handleChange}
+          />
+        )}
         <textarea
           name="content"
           placeholder="Take a note..."
           value={note.content}
-          rows="3"
+          rows={isExpanded ? 3 : 1}
           onChange={handleChange}
+          onFocus={shouldExpand}
         />
-        <button
-          onClick={(event) => {
-            props.onCreate(note);
-            setNote({
-              title: "",
-              content: "",
-            });
+        <Zoom in={isExpanded}>
+          <Fab
+            onClick={(event) => {
+              props.onCreate(note);
+              setNote({
+                title: "",
+                content: "",
+              });
 
-            // Prevents the page from reloading
-            event.preventDefault();
-          }}
-        >
-          Add
-        </button>
+              setExpanded(false);
+              // Prevents the page from reloading
+              event.preventDefault();
+            }}
+          >
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Pin from "@material-ui/icons/Highlight";
 
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1>
+        <Pin />
+        Keeper
+      </h1>
     </header>
   );
 }
